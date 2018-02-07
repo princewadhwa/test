3d239a659b17f6b6cd76d02da15c74aef27c5b87
Ubuntu <ubuntu@ip-10-0-0-141.ec2.internal>
2018-02-07 10:06:26 +0000

    index.php commit

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello ECS world!</title>
+	<title>Hello ECS world! How are you!!</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
